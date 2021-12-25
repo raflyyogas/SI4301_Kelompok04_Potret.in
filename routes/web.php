21eb1ce8d697fd280b995af2bfded9744d62f177
Rafly Yogaswara <raflyyogaswara48@gmail.com>
2021-12-25 11:47:10 +0700

    Back To Fresh Build

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', function () {
-    return view('Rafly_home',[
-        "title" => "Home"
-    ]);
+    return view('welcome');
 });
-Route::resource('vaccine',Rafly_vaccinessControllers::class,);
-Route::resource('patient',Rafly_patientsControllers::class,);
-Route::get('/patient/{vaccine}/create','Rafly_patientsControllers@create');
