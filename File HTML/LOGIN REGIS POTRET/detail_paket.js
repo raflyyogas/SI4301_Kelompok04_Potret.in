22977b52d4d3abd8d5ddceb4a6ad8d7e53f53948
shobrunjb <shobrunjb@gmail.com>
2022-01-13 19:23:17 +0700

    New Commit

---
+++ File HTML/LOGIN REGIS POTRET/detail_paket.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // active thumbnail
+  $("#thumbSlider .thumb").on("click", function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
+});
